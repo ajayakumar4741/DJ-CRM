daae4ad04938151e3ed04ca90979df3f7cf08c3e
Ajaya Kumar <ajayakumar4741@gmail.com>
2025-09-02 17:41:40 +0530

    Added new things

--- crm/models.py
+++ crm/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Records(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    city = models.CharField(max_length=40)
+    country = models.CharField(max_length=40)
+    zipcode = models.CharField(max_length=30)
+    
+    class Meta:
+        verbose_name_plural = 'Records'
+    
+    def __str__(self):
+        return(f'{self.first_name} {self.last_name}')
+    
--- crm/views.py
+++ crm/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from . forms import RegForm
+from . models import *
 # Create your views here.
+
+
+def index(request):
+    records = Records.objects.all()
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(username=username, password=password)
+        # Check if user logged in
+        if user:
+            login(request, user)
+            messages.success(request,'Successfully logged in!')
+            return redirect('index')
+        else:
+            messages.error(request,'Invalid Credentials')
+            return redirect('index')
+    return render(request,'index.html',{'records':records})
+
+def record(request,pk):
+    pass
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,'You Have Been Logged Out...')
+    return redirect('index')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = RegForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'You Have Successfully Registered...')
+            return redirect('index')
+    else:
+        form = RegForm()
+    return render(request,'reg.html',{'form':form})
+    # return render(request,'reg.html',{'form':form})
