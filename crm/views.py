917192775245b26450fb62306881aa3c7cb61099
Ajaya Kumar <ajayakumar4741@gmail.com>
2025-09-03 12:29:46 +0530

    Added few more things

--- crm/views.py
+++ crm/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from . forms import RegForm
+from . forms import RegForm, AddRecordForm
 from . models import *
 # Create your views here.
 
@@ -24,7 +24,50 @@ def index(request):
     return render(request,'index.html',{'records':records})
 
 def record(request,pk):
-    pass
+    if request.user.is_authenticated:
+        customer_record = Records.objects.get(id=pk)
+        return render(request,'record.html',{'customer_record':customer_record})
+    else:
+        messages.error(request,'You Must Be Logged in to access that page...')
+        return redirect('index')
+    
+def delete_record(request,pk):
+    if request.user.is_authenticated:
+        delete_item = Records.objects.get(id=pk)
+        delete_item.delete()
+        messages.success(request,'Record Deleted Successfully...')
+        return redirect('index')
+    else:
+        messages.error(request,'You Must Be Logged in to delete records...')
+        return redirect('index')
+    
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Record Added...')
+                return redirect('index')
+        else:
+            return render(request,'add.html',{'form':form})
+    else:
+        messages.error(request,'You Must Be Logged in to add records...')
+        return redirect('index')
+
+def update_record(request,pk):
+    if request.user.is_authenticated:
+        current_user = Records.objects.get(id=pk) 
+        form = AddRecordForm(request.POST or None, instance=current_user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Record Updated...')
+            return redirect('index')
+        else:
+            return render(request,'update.html',{'form':form})
+    else:
+        messages.error(request,'You Must Be Logged in to update records...')
+        return redirect('index')
 
 def logout_user(request):
     logout(request)
